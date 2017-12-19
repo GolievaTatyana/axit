ea79f2d6b809592a9d317348f59cb9538189e559
Tanya <golieva.tatyana@gmail.com>
2017-12-19 14:09:48 +0200

    add favicon

--- theme/header.php
+++ theme/header.php
@@ -6,6 +6,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="shortcut icon" href="<?php echo $templateUri ?>/favicon.ico" type="image/x-icon">
   <link rel="stylesheet" href="<?php echo $templateUri ?>/css/bootstrap.min.css">
   <link rel="stylesheet" href="<?php echo $templateUri ?>/style.css">
   <link rel="stylesheet" href="<?php echo $templateUri ?>/css/jquery.mCustomScrollbar.css">
